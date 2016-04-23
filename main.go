e2f739f4b9ef6690a9c064e3f1204e6be54deeba
Erik Craddock <Erik@Craddock.org>
2016-04-22 22:42:02 -0500

    added cors handling

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/codegangsta/negroni"
 	"github.com/evcraddock/supp-store/config"
 	"github.com/evcraddock/supp-store/routers"
+	//"github.com/gorilla/mux"
 )
 
 func main() {
@@ -19,7 +20,30 @@ func main() {
 	n := negroni.Classic()
 
 	fmt.Printf("start service on port %v \n", websiteAddress)
-	n.UseHandler(router)
+
+	n.UseHandler(&router)
 	log.Fatal(http.ListenAndServe(websiteAddress, n))
 
 }
+
+// type WithCORS struct {
+// 	r *mux.Router
+// }
+
+// func (s *WithCORS) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+// 	if origin := req.Header.Get("Origin"); origin != "" {
+// 		res.Header().Set("Access-Control-Allow-Origin", origin)
+// 		res.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+// 		res.Header().Set("Access-Control-Allow-Headers",
+// 			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+// 	}
+
+// 	//.Printf("%v", origin)
+// 	// Stop here for a Preflighted OPTIONS request.
+// 	if req.Method == "OPTIONS" {
+// 		fmt.Printf("handled options")
+// 		return
+// 	}
+// 	// Lets Gorilla work
+// 	s.r.ServeHTTP(res, req)
+// }
--- routers/routers.go
+++ routers/routers.go
@@ -2,11 +2,31 @@ package routers
 
 import (
 	"github.com/gorilla/mux"
+	"net/http"
 )
 
-func InitRoutes() *mux.Router {
+func InitRoutes() CORSHandler {
 	router := mux.NewRouter()
 	router = SetSupplementEntryRoutes(router)
 
-	return router
+	return CORSHandler{router}
+}
+
+type CORSHandler struct {
+	r *mux.Router
+}
+
+func (s *CORSHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	if origin := req.Header.Get("Origin"); origin != "" {
+		res.Header().Set("Access-Control-Allow-Origin", origin)
+		res.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		res.Header().Set("Access-Control-Allow-Headers",
+			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	}
+
+	if req.Method == "OPTIONS" {
+		return
+	}
+
+	s.r.ServeHTTP(res, req)
 }
